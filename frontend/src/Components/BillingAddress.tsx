1ccc846c9d9bce3f6e9be13c62db7226a059e8af
LPF33 <larspefe@gmail.com>
2020-10-19 15:23:05 +0200

    Shipping/Billing component accessibility

--- frontend/src/Components/BillingAddress.tsx
+++ frontend/src/Components/BillingAddress.tsx
@@ -31,61 +31,67 @@ const BillingAddress: React.FC = () => {
         <AddressWrapper>
             <AddressForm>
                 <h3>Billing Address</h3>
-                <label>Your last name</label>
+                <label htmlFor={EnumAddress.lastname}>Your last name</label>
                 {error.lastname && <span>{error.lastname}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.lastname}
                     name={EnumAddress.lastname}
                     value={addressState.lastname}
                     onChange={handleChange}
                     className={error.lastname ? "empty" : ""}
                 />
-                <label>Your first name</label>
+                <label htmlFor={EnumAddress.firstname}>Your first name</label>
                 {error.firstname && <span>{error.firstname}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.firstname}
                     name={EnumAddress.firstname}
                     value={addressState.firstname}
                     onChange={handleChange}
                     className={error.firstname ? "empty" : ""}
                 />
-                <label>Street name</label>
+                <label htmlFor={EnumAddress.streetname}>Street name</label>
                 {error.streetname && <span>{error.streetname}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.streetname}
                     name={EnumAddress.streetname}
                     value={addressState.streetname}
                     onChange={handleChange}
                     className={error.streetname ? "empty" : ""}
                 />
-                <label>Street number</label>
+                <label htmlFor={EnumAddress.streetnumber}>Street number</label>
                 {error.streetnumber && <span>{error.streetnumber}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.streetnumber}
                     name={EnumAddress.streetnumber}
                     value={addressState.streetnumber}
                     onChange={handleChange}
                     className={error.streetnumber ? "empty" : ""}
                 />
-                <label>ZIP Code</label>
+                <label htmlFor={EnumAddress.postalcode}>ZIP Code</label>
                 {error.postalcode && <span>{error.postalcode}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.postalcode}
                     name={EnumAddress.postalcode}
                     value={addressState.postalcode}
                     onChange={handleChange}
                     className={error.postalcode ? "empty" : ""}
                 />
-                <label>City name</label>
+                <label htmlFor={EnumAddress.city}>City name</label>
                 {error.city && <span>{error.city}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.city}
                     name={EnumAddress.city}
                     value={addressState.city}
                     onChange={handleChange}
--- frontend/src/Components/Shipping.tsx
+++ frontend/src/Components/Shipping.tsx
@@ -35,71 +35,78 @@ const Shipping: React.FC = () => {
         <AddressWrapper>
             <AddressForm>
                 <h3>Shipping Address</h3>
-                <label>Your last name</label>
+                <label htmlFor={EnumAddress.lastname}>Your last name</label>
                 {error.lastname && <span>{error.lastname}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.lastname}
                     name={EnumAddress.lastname}
                     value={addressState.lastname}
                     onChange={handleChange}
                     className={error.lastname ? "empty" : ""}
                 />
-                <label>Your first name</label>
+                <label htmlFor={EnumAddress.firstname}>Your first name</label>
                 {error.firstname && <span>{error.firstname}</span>}
                 <input
                     type="text"
+                    id={EnumAddress.firstname}
                     autoComplete="off"
                     name={EnumAddress.firstname}
                     value={addressState.firstname}
                     onChange={handleChange}
                     className={error.firstname ? "empty" : ""}
                 />
-                <label>Street name</label>
+                <label htmlFor={EnumAddress.streetname}>Street name</label>
                 {error.streetname && <span>{error.streetname}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.streetname}
                     name={EnumAddress.streetname}
                     value={addressState.streetname}
                     onChange={handleChange}
                     className={error.streetname ? "empty" : ""}
                 />
-                <label>Street number</label>
+                <label htmlFor={EnumAddress.streetnumber}>Street number</label>
                 {error.streetnumber && <span>{error.streetnumber}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.streetnumber}
                     name={EnumAddress.streetnumber}
                     value={addressState.streetnumber}
                     onChange={handleChange}
                     className={error.streetnumber ? "empty" : ""}
                 />
-                <label>ZIP Code</label>
+                <label htmlFor={EnumAddress.postalcode}>ZIP Code</label>
                 {error.postalcode && <span>{error.postalcode}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.postalcode}
                     name={EnumAddress.postalcode}
                     value={addressState.postalcode}
                     onChange={handleChange}
                     className={error.postalcode ? "empty" : ""}
                 />
-                <label>City name</label>
+                <label htmlFor={EnumAddress.city}>City name</label>
                 {error.city && <span>{error.city}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.city}
                     name={EnumAddress.city}
                     value={addressState.city}
                     onChange={handleChange}
                     className={error.city ? "empty" : ""}
                 />
-                <label>Your email:</label>
+                <label htmlFor={EnumAddress.email}>Your email:</label>
                 {error.email && <span>{error.email}</span>}
                 <input
                     type="text"
                     autoComplete="off"
+                    id={EnumAddress.email}
                     name={EnumAddress.email}
                     value={addressState.email}
                     onChange={handleChange}
@@ -109,13 +116,16 @@ const Shipping: React.FC = () => {
                 <div>
                     <input
                         type="checkbox"
+                        id="secondAddress"
                         checked={secondAddress}
                         onChange={() => {
                             const val: boolean = !secondAddress;
                             dispatch(setSecondAddress(val));
                         }}
                     />
-                    <label>I have a different billing address</label>
+                    <label htmlFor="secondAddress">
+                        I have a different billing address
+                    </label>
                 </div>
             </AddressForm>
             <Next type="button" onClick={nextPage}>
